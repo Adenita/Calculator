2379a30e7b781af3472ff2d62bf6925a23e2d715
adenita <nittaluta@gmail.com>
2024-03-07 00:49:27 +0100

    refactored board to include new changes

--- src/components/BoardComponent/Board.js
+++ src/components/BoardComponent/Board.js
@@ -1,31 +1,36 @@
 import './Board.css';
 import Button from "../ButtonComponent/Button";
-export default function Board() {
+import Field from "../FieldComponent/Field";
+export default function Board({result, onButtonClick }) {
     return (
         <div className="board">
             <div>
-                <Button value="7"></Button>
-                <Button value="8"></Button>
-                <Button value="9"></Button>
-                <Button value="/"></Button>
+                <Field result={result}></Field>
+                <Button value="C" onButtonClick={onButtonClick}/>
             </div>
             <div>
-                <Button value="4"></Button>
-                <Button value="5"></Button>
-                <Button value="6"></Button>
-                <Button value="x"></Button>
+                <Button value="7" onButtonClick={onButtonClick}/>
+                <Button value="8" onButtonClick={onButtonClick} />
+                <Button value="9" onButtonClick={onButtonClick} />
+                <Button value="/" onButtonClick={onButtonClick} />
             </div>
             <div>
-                <Button value="1"></Button>
-                <Button value="2"></Button>
-                <Button value="3"></Button>
-                <Button value="-"></Button>
+                <Button value="4" onButtonClick={onButtonClick} />
+                <Button value="5" onButtonClick={onButtonClick} />
+                <Button value="6" onButtonClick={onButtonClick} />
+                <Button value="*" onButtonClick={onButtonClick} />
             </div>
             <div>
-                <Button value="0"></Button>
-                <Button value="."></Button>
-                <Button value="+"></Button>
-                <Button value="="></Button>
+                <Button value="1" onButtonClick={onButtonClick} />
+                <Button value="2" onButtonClick={onButtonClick} />
+                <Button value="3" onButtonClick={onButtonClick} />
+                <Button value="-" onButtonClick={onButtonClick} />
+            </div>
+            <div>
+                <Button value="0" onButtonClick={onButtonClick} />
+                <Button value="." onButtonClick={onButtonClick} />
+                <Button value="+" onButtonClick={onButtonClick} />
+                <Button value="=" onButtonClick={onButtonClick} />
             </div>
         </div>
     );
